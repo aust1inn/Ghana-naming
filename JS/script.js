0ae30d84d2a0ec4839684a762fae154f627588a6
aust1inn <austinotieno364@gmail.com>
2022-03-03 18:10:40 +0300

    sets the if statements

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,74 @@
 function check () {
-    let inputtedDay = document.getElementById("day").value
-    let inputtedMonth = document.getElementById("month").value
-    let inputtedYear = document.getElementById("year").value
+    let inputtedDay = document.getElementById("day").value;
+    let inputtedMonth = document.getElementById("month").value;
+    let inputtedYear = document.getElementById("year").value;
+    let gender = document.getElementById("gender").value;
+
 
     let stringDay = inputtedDay.toString();
     let stringMonth = inputtedMonth.toString();
     let stringYear = inputtedYear.toString();
 
-    let ourDay = stringDay + "-" + stringMonth + "-" + stringYear;
+    let ourDay = stringMonth + "-" + stringDay + "-" + stringYear;
     let p = new Date(ourDay)
     let dayWeek = p.getDay();
-    alert (dayWeek)
+
+    // alert (dayWeek)
+    // alert(gender)
+    
+    if (dayWeek==0 && gender=="female") {
+        alert("yo")
+    }
+    else if (dayWeek==1 && gender=="female") {
+        
+    }
+
+    else if (dayWeek==1 && gender=="female") {
+        
+    }
+
+    else if (dayWeek==1 && gender=="female") {
+        
+    }
+
+    else if (dayWeek==1 && gender=="female") {
+        
+    }
 
     
+    else if (dayWeek==1 && gender=="female") {
+        
+    }
+
+    else if (dayWeek==1 && gender=="female") {
+        
+    }
+
+    else if (dayWeek==1 && gender=="male") {
+        
+    }
+
+    else if (dayWeek==1 && gender=="male") {
+        
+    }
+
+    else if (dayWeek==1 && gender=="male") {
+        
+    }
+
+    else if (dayWeek==1 && gender=="male") {
+        
+    }
+
+    else if (dayWeek==1 && gender=="male") {
+        
+    }
+
+    else if (dayWeek==1 && gender=="male") {
+        
+    }
+    else {
+        
+    }
+
 }
